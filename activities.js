fc30d4adf9126c978aaf71675d1a0db58963c55b
K Shuck <rom476@live.missouristate.edu>
2021-02-23 11:26:00 -0600

    All RBs
    All radiobuttons now display the correct checkboxes. That was tedious. Next push will remove old checkboxes.

--- activities.js
+++ activities.js
@@ -5,8 +5,7 @@ function displayRadioValue() {
         if(ele[i].checked)
         {
             radioButtonVal = ele[i].value;
-            //document.write(ele[i].value);
-            //document.write("Yup: " + radioButtonVal);
+            
             if(radioButtonVal == "cancun")
             {
 
@@ -84,19 +83,234 @@ function displayRadioValue() {
             }
             else if(radioButtonVal == "venice")
             {
+                //Assigning code to the Div tag labeled result
+                var result = document.getElementById("result");
+
+                //First CheckBox for Venice
+                var checkbox4 = document.createElement('input');
+                checkbox4.type = "checkbox";
+                checkbox4.name = "resort";
+                checkbox4.value = "museums";
+             
+                //Label for CheckBox 4 (CB1 for Venice)
+                var label4 = document.createElement('label4')
+                label4.appendChild(document.createTextNode('Museums'));
+
+
+                //2
+                var checkbox5 = document.createElement('input');
+                checkbox5.type = "checkbox";
+                checkbox5.name = "resort";
+                checkbox5.value = "theatre";
+                
+
+                //2
+                var label5 = document.createElement('label5')
+                label5.appendChild(document.createTextNode('Theatre'));
+
+                //3
+                var checkbox6 = document.createElement('input');
+                checkbox6.type = "checkbox";
+                checkbox6.name = "resort";
+                checkbox6.value = "parksandrecreation";
+                
+
+                //3
+                var label6 = document.createElement('label6')
+                label6.appendChild(document.createTextNode('Parks and Recreation'));
+
+                //4
+                var checkbox7 = document.createElement('input');
+                checkbox7.type = "checkbox";
+                checkbox7.name = "resort";
+                checkbox7.value = "citytours";
+                
+
+                //4
+                var label7 = document.createElement('label7')
+                label7.appendChild(document.createTextNode('City Tours'));
+                
+                //Append
+                linebreak = document.createElement("br");
+
+                result.appendChild(checkbox4);
+                result.appendChild(label4);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox5);
+                result.appendChild(label5);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox6);
+                result.appendChild(label6);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox7);
+                result.appendChild(label7);
+                result.appendChild(linebreak);
 
             }
             else if(radioButtonVal == "maldives")
             {
+                 //Assigning code to the Div tag labeled result
+                 var result = document.getElementById("result");
 
+                 //First CheckBox for maldives
+                 var checkbox8 = document.createElement('input');
+                 checkbox8.type = "checkbox";
+                 checkbox8.name = "resort";
+                 checkbox8.value = "museums";
+              
+                 //Label for CheckBox 8 (CB1 for maldives)
+                 var label8 = document.createElement('label8')
+                 label8.appendChild(document.createTextNode('Museums'));
+
+                 //2
+                 var checkbox9 = document.createElement('input');
+                 checkbox9.type = "checkbox";
+                 checkbox9.name = "resort";
+                 checkbox9.value = "sailing";
+              
+                 //2
+                 var label9 = document.createElement('label9')
+                 label9.appendChild(document.createTextNode('Sailing'));
+
+                 //3
+                 var checkbox10 = document.createElement('input');
+                 checkbox10.type = "checkbox";
+                 checkbox10.name = "resort";
+                 checkbox10.value = "beach";
+              
+                 //3
+                 var label10 = document.createElement('label10')
+                 label10.appendChild(document.createTextNode('Beach'));
+
+                 //4
+                 var checkbox11 = document.createElement('input');
+                 checkbox11.type = "checkbox";
+                 checkbox11.name = "resort";
+                 checkbox11.value = "hiking";
+              
+                 //4
+                 var label11 = document.createElement('label11')
+                 label11.appendChild(document.createTextNode('Hiking'));
+
+                  //4
+                  var checkbox12 = document.createElement('input');
+                  checkbox12.type = "checkbox";
+                  checkbox12.name = "resort";
+                  checkbox12.value = "boating";
+               
+                  //4
+                  var label12 = document.createElement('label12')
+                  label12.appendChild(document.createTextNode('Boating'));
+
+                 //Append
+                linebreak = document.createElement("br");
+
+                result.appendChild(checkbox8);
+                result.appendChild(label8);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox9);
+                result.appendChild(label9);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox10);
+                result.appendChild(label10);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox11);
+                result.appendChild(label11);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox12);
+                result.appendChild(label12);
+                result.appendChild(linebreak);
             }
             else if(radioButtonVal == "newzealand")
             {
+                //Assigning code to the Div tag labeled result
+                var result = document.getElementById("result");
+
+                //First CheckBox for NewZealand
+                var checkbox13 = document.createElement('input');
+                checkbox13.type = "checkbox";
+                checkbox13.name = "resort";
+                checkbox13.value = "citytours";
+               
+                //Label for CheckBox 8 (CB1 for maldives)
+                var label13 = document.createElement('label13')
+                label13.appendChild(document.createTextNode('City Tours')); 
+
+                //2
+                var checkbox14 = document.createElement('input');
+                checkbox14.type = "checkbox";
+                checkbox14.name = "resort";
+                checkbox14.value = "sports";
+               
+                //2
+                var label14 = document.createElement('label14')
+                label14.appendChild(document.createTextNode('Sports')); 
+
+                 //3
+                 var checkbox15 = document.createElement('input');
+                 checkbox15.type = "checkbox";
+                 checkbox15.name = "resort";
+                 checkbox15.value = "cycling";
+                
+                 //3
+                 var label15 = document.createElement('label15')
+                 label15.appendChild(document.createTextNode('Cycling'));
+
+                 //4
+                 var checkbox16 = document.createElement('input');
+                 checkbox16.type = "checkbox";
+                 checkbox16.name = "resort";
+                 checkbox16.value = "museums";
+                
+                 //4
+                 var label16 = document.createElement('label17')
+                 label16.appendChild(document.createTextNode('Museums')); 
+
+                 //5
+                 var checkbox17 = document.createElement('input');
+                 checkbox17.type = "checkbox";
+                 checkbox17.name = "resort";
+                 checkbox17.value = "boating";
+                
+                 //5
+                 var label17 = document.createElement('label17')
+                 label17.appendChild(document.createTextNode('Boating')); 
+ 
+
+                //Append
+                linebreak = document.createElement("br");
+
+                result.appendChild(checkbox13);
+                result.appendChild(label13);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox14);
+                result.appendChild(label14);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox15);
+                result.appendChild(label15);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox16);
+                result.appendChild(label16);
+                result.appendChild(linebreak);
+
+                result.appendChild(checkbox17);
+                result.appendChild(label17);
+                result.appendChild(linebreak);
 
             }
             else
             {
-
+                document.write("Invalid Selection");
             }
             
         }
